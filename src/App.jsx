4154936d506c07579830c6d4e7369f426f5b0719
VinayPrajapati04 <vp301334@gmail.com>
2025-11-01 23:49:23 +0530

    Add Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import IntroSection from './components/introComponent/IntroSection'
 import Parallax from './components/parallax/Parallax'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/Contact/Contact'
+// import Contact from './components/Contact/Contact'
+import Contact from 'src/components/Contact/Contact'
+
 import Cursor from './components/cursor/Cursor'
 
 const App = () => {
