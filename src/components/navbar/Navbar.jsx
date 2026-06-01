c6cf5cb79c65b5ed80173b68124c827e0e8c6ba7
Vinay Daksh <vinayprajapati0204@gmail.com>
2026-06-01 23:11:07 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           animate={{ opacity: 1, scale:1 }}
           transition={{ duration: 0.5 }}
         >
-         Vinay Prajapati
+         Vinay Daksh
         </motion.span>
         <div className="social">
           <a href="">
